87b02f62ea41cc3bf2591d824893dee1e60d2b50
Vijay S <37852613+vijay043077@users.noreply.github.com>
2020-04-29 00:02:54 -0500

    Hiding APIs

--- WeatherPy/starter_code/api_keys.py
+++ WeatherPy/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "544c6313fcf4597fc1db588c34475f42"
+weather_api_key = "#############################5f42"
 
 # Google API Key
-g_key = "AIzaSyBUKnl2UTYNJNVv4YUKZn-lG0zxbtMzH6Q"
+g_key = "##################################MzH6Q"
